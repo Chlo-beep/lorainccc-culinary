90a088a62ab093f6dd3013da171c0a1510f2608f
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-03-08 17:31:15 -0500

    :wrench: Intitial Commit

---
+++ template-parts/nav-offcanvas.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Custom Google Search bar
+if( is_active_sidebar('lccc-search-sidebar') ) :
+
+	dynamic_sidebar('lccc-search-sidebar');
+
+endif;
+
+culinary_main_nav();
+
+culinary_top_nav(); 
+
+?>
